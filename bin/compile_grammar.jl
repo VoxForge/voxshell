76413fb7d073e5d450146f55804072fcf2580653
Ken MacLean <kmaclean@voxforge.org>
2015-07-26 16:17:08 -0400

    update comnet

--- bin/compile_grammar.jl
+++ bin/compile_grammar.jl
@@ -146,6 +146,9 @@ function vfvoca2dict(vocafile,dic,dictfile)
     # compound words are seperated by dashes, but pronunciation is looked for 
     # each individual word, and joined together with short pause (sp) between
     # them
+    # Note: no need to check VoxForgeDict if word is alraedy defined in dict,
+    # because if it is, then the triphones you need are already defined, and 
+    # that's all that matters
     function process_compound_word(command, word)
       compound_word=split(word,r"\-")
       write(dictfile_fh, "$(newid)\t$command ")
